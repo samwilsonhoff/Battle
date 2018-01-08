e47bf09faded69d4d338a338470b90ef1c50d242
Theo Auge <theoauge0@gmail.com>
2018-01-08 13:58:12 -0600

    testing

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-function setup() {
-  createCanvas(600, 600);
-}
-
-function draw() {
-  background(51);
-}
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(0,0,150,75);

ef6e97f8b80b0cc534d8415fd84a22ff342417ba
Theo Auge <theoauge0@gmail.com>
2018-03-01 10:44:26 -0600

    extra file

--- main.js
+++
@@ -1,9 +0,0 @@
-var canvas = document.getElementById("canvas");
-var c = canvas.getContext("2d");
-var width = 300;
-var height = 300;
-
-function background() {
-  c.fillRect(0, 0, width, height);
-  c.fillStyle = "#FF0000";
-}
